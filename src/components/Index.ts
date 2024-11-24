162b3fdce5e93c5fc426983124bfc7b4e402fa12
Rasim <eferiums@yandex.ru>
2024-11-24 17:43:18 +0400

    refactor: Рефакторинг кода p.1.3

--- src/components/Index.ts
+++ src/components/Index.ts
@@ -1,21 +1,15 @@
 export { default as GoBackHome } from './UI/GoBackHome/GoBackHome';
 export { default as SubmitButtonBlack } from './UI/SubmitButton/SubmitButtonBlack';
 export { default as SubmitButtonWhite } from './UI/SubmitButton/SubmitButtonWhite';
-
+export { default as NavButtonBlack } from './UI/NavButton/NavButtonBlack';
+export { default as NavButtonWhite } from './UI/NavButton/NavButtonWhite';
+export { default as NavButtonIconRed } from './UI/NavButtonIcon/NavButtonIconRed';
 export { default as Account } from './Account/Account';
 export { default as Footer } from './Footer/Footer';
 export { default as Header } from './Header/MainHeader';
-
 export { default as Modal } from './UI/Modal/Modal';
-
-export { default as NavButtonIconRed } from './UI/NavButtonIcon/NavButtonIconRed';
 export { default as Navigation } from './UI/Navigation/Navigation';
-
-export { default as NavButtonBlack } from './UI/NavButton/NavButtonBlack';
-export { default as NavButtonWhite } from './UI/NavButton/NavButtonWhite';
-
 export { default as MainHeader } from './Header/MainHeader';
-
 export { default as RecipeDetalis } from './Recipes/Recipe/components/RecipeDetails/RecipeDetalis';
 export { default as RecipePreview } from './Recipes/Recipe/components/RecipePreview/RecipePreview';
 export { default as RecipeStep } from './Recipes/Recipe/components/RecipeStep/RecipeStep';
