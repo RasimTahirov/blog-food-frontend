88decbc8e341f08938141912d4c1656ac87aa65c
Rasim <eferiums@yandex.ru>
2024-12-10 22:43:15 +0400

    fix: build fix p.1

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,12 @@ export default defineConfig({
     port: 3000,
     strictPort: true
   },
-  base: '/'
+  base: '/',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./styles/styles.scss"`
+      }
+    }
+  }
 });
